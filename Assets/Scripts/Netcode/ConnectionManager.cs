5be1b5dab26e2949c8a786001916fd24eeffcbd3
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2023-03-10 10:00:29 -0800

    Ensured players would always be a unique color
    Before if players connected and disconnected enough, they would eventually all default to the grey astronaut. Additionally, if the approach depended directly on the player count, there was a chance that multiple people would be the same main color (i.e., 1 red + 2 greens). This approach ensures everyone is a different non-grey color as long as there are 4 or less people

--- Assets/Scripts/Netcode/ConnectionManager.cs
+++ Assets/Scripts/Netcode/ConnectionManager.cs
@@ -10,14 +10,22 @@ namespace SKGG.Netcode
         private NetworkManager networkManager => NetworkManager.Singleton;
 
         [SerializeField] GameObject[] playerPrefabs;
-        private int numPlayers;
+        private ulong[] costumeOwners;
 
         //don't do things with the network manager in awake
         private void Start()
         {
+            costumeOwners = new ulong[4];
             networkManager.OnClientConnectedCallback += SpawnPlayer;
             networkManager.NetworkConfig.PlayerPrefab = null; //the player prefab should be spawned by this script, not by NetworkManager
-            numPlayers = 0;
+            networkManager.OnClientDisconnectCallback += DespawnPlayer;
+        }
+
+        //note: don't try to convert this to an InitServer function, network manager calls "OnServerStarted" AFTER "OnClientConnected",
+        //so it won't properly account for the host
+        private void EndServer()
+        {
+            costumeOwners = new ulong[4];
         }
 
         private void SpawnPlayer(ulong clientId)
@@ -25,13 +33,56 @@ namespace SKGG.Netcode
             //only the server has the authority to spawn a player
             if (!networkManager.IsServer)
                 return;
-            //increment numPlayers AFTER setting playerCostumeID so that the id starts at 0
-            int playerCostumeID = numPlayers;
-            if (numPlayers > 4)
-                playerCostumeID = 4; //grey
+            //Debug.Log($"Connecting client {clientId}");
+            int playerCostumeID = GetNewPlayerCostume();
+            if(playerCostumeID == -1) //no costumes are available
+            {
+                ReevaluatePlayers(); //make sure costume availability is properly updated
+                playerCostumeID = GetNewPlayerCostume(); //try again
+                if(playerCostumeID == -1)
+                {
+                    //should disconnect player, but I'm not doing that yet
+                    playerCostumeID = 4;
+                }
+            }
+            costumeOwners[playerCostumeID] = clientId+1; //the +1 is so that 0 will 100% mean "unused"
             NetworkObject newPlayerObject = Instantiate(playerPrefabs[playerCostumeID]).GetComponent<NetworkObject>();
             newPlayerObject.SpawnAsPlayerObject(clientId);
-            numPlayers++;
+        }
+
+        private void DespawnPlayer(ulong clientId)
+        {
+            //only the server has the authority to despawn a player
+            if (!networkManager.IsServer)
+                return;
+            for (int i = 0; i < costumeOwners.Length; i++)
+            {
+                if(costumeOwners[i] == clientId+1)
+                {
+                    costumeOwners[i] = 0;
+                }
+            }
+        }
+
+        private int GetNewPlayerCostume()
+        {
+            for (int i = 0; i < costumeOwners.Length; i++)
+            {
+                if (costumeOwners[i] == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void ReevaluatePlayers()
+        {
+            for (int i = 0; i < costumeOwners.Length; i++)
+            {
+                if (networkManager.ConnectedClients[costumeOwners[i]] == null)
+                    costumeOwners[i] = 0;
+            }
         }
     }
 }
